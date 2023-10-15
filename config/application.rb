994a8da75c8cdad57672acd1629b728e8f9f4a78
maiko <urbanrepablic@gmail.com>
2023-10-15 14:34:24 +0900

    add local's setting in application.rb

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module CdpWebManyoTask
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.i18n.default_locale = :ja
+    config.i18n.locale = :ja
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
     config.generators do |g|
